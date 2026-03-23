aa49f1623562f8689eec4caff48cfc144fb718e4
hordzich ihar <Garik666770@gmail.com>
2026-03-23 23:32:17 +0300

    fix react-loader-spinner

--- components/Loader/Loader.tsx
+++ components/Loader/Loader.tsx
@@ -1,10 +1,9 @@
-import { RotatingLines } from 'react-loader-spinner';
 import css from './Loader.module.css';
 
 export default function Loader() {
   return (
     <div className={css.loader} role="status" aria-label="Loading">
-      <RotatingLines strokeColor="#0d6efd" width="32" />
+      <span className={css.spinner} />
     </div>
   );
 }
