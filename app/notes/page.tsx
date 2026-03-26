097a007ed43d174d16964c25c38989d2b44522d8
hordzich ihar <Garik666770@gmail.com>
2026-03-26 21:18:40 +0300

    fix codr 3

--- app/notes/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function NotesPage() {
-  // Базовый маршрут /notes перенаправляем на фильтр "all".
-  redirect("/notes/filter/all");
-}
